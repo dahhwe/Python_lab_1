d081557e0d87fa49b205254bc277e249b1084192
dahhwe <68936213+dahhwe@users.noreply.github.com>
2022-04-14 10:51:05 +0700

    Add files via upload

---
+++ lab_7/employees.py
@@ -0,0 +1,27 @@
+"""
+Класс с информацией о сотрудниках
+"""
+
+
+class Employee:
+    """
+    Инициализация атрибутов сотрудников
+    """
+    def __init__(self, first_name, last_name, workplace, position, salary):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__workplace = workplace
+        self.__position = position
+        self.__salary = salary
+        self.__email = first_name+'.'+last_name+'@SuccessfulBusiness.co.uk'
+
+    def show_data(self):
+        """
+        Вывод информации о сотрудниках
+        :return:
+        """
+        print("Id\t\t:", self.__first_name)
+        print("Name\t:", self.__last_name)
+        print("Workplace\t:", self.__workplace)
+        print("Position\t:", self.__position)
+        print("Salary\t:", self.__salary)
---
+++ lab_7/workplace.py
@@ -0,0 +1,24 @@
+"""
+Класс с отделами работ
+"""
+
+
+class workplace:
+    def __init__(self, name, address, location, requirements):
+        """
+        Инициализация атрибутов отделов
+        """
+        self.__name = name
+        self.__address = address
+        self.__location = location
+        self.__requirements = requirements
+
+    def workplace_information(self):
+        """
+        Вывод информации о отделах работ
+        :return:
+        """
+        print("Name\t:", self.__name)
+        print("Address\t\t:", self.__address)
+        print("Location\t:", self.__location)
+        print("Requirements\t:", self.__requirements)
